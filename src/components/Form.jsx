ceee68e7f01cae3016e825263ae60cec39807a0b
Shreyoshi Das <shreyoshi4119@gmail.com>
2025-11-09 13:01:12 +0530

    new commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -37,13 +37,16 @@ export default function Form() {
     setMessage("");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict_persona", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://wellness-persona-api-2.onrender.com/predict_persona",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       if (!response.ok) {
         throw new Error("Failed to send data");
